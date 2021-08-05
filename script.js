67985c57582635645eba967cb488e07ee98d4cd8
Joey <30815220+joeylking@users.noreply.github.com>
2021-08-05 13:39:30 -0500

    Fetch quotes and pick random

---
+++ script.js
@@ -0,0 +1,22 @@
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+  // Pick random quote from API array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get quotes from API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getQuotes();
